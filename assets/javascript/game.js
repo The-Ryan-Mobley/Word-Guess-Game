ca1a7d08328f96583b0847812c50ea6662dc6a2d
Ryan Mobley <ryan.mobley94@gmail.com>
2019-06-27 20:03:33 -0500

    developing javascript functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,26 @@
 class word{
-    constructor(n, m){
-        this.name=n;
-        this.media=document.getElementById("myAudio"); ;
+    constructor(n){
+        this.name = n;
+        //this.media=document.getElementById("myAudio"); ;
         this.guesses=5;
     }
     draw(){
+        let cha = Array.from(this.name);
+        for(let i=0; i<cha.length; i++){
+            document.write(cha[i] + " ");
+        }
 
     }
     fail(){
         this.guesses--;
     }
+    // playmusic(){
+    //     this.media.play();
+    // }
 
 }
+
+var wordlist = ["test", "x", "y","hello","world"];
+rand = Math.floor(Math.random() * wordlist.length);
+var x = new word(wordlist[rand]);
+x.draw();
