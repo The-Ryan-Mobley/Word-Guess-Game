4ee235e69802619c155edfdba3151a61146e9e0d
Ryan Mobley <ryan.mobley94@gmail.com>
2019-06-30 16:46:06 -0500

    core functions of the game are complete, now implimenting audio and img files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,59 @@
+var guessword = document.getElementById("guessspot");
+var underline = document.getElementById("underlines");
+var winloss = document.getElementById("winstate");
+
+
 class word{
     constructor(n){
         this.name = n;
-        //this.media=document.getElementById("myAudio"); ;
+        this.themesong = document.getElementById("sound");
         this.guesses=5;
+        this.dummysentence =[];
+
     }
     draw(){
-        let cha = Array.from(this.name);
-        for(let i=0; i<cha.length; i++){
-            document.write(cha[i] + " ");
+        
+        for(let i=0; i<this.name.length; i++){
+            this.dummysentence[i]="_ ";
+            
         }
-
+        guessword.textContent = this.dummysentence;
     }
-    fail(){
-        this.guesses--;
+    rightguess(letter){ //if player presses a correct key it will redraw the word
+        if(x.name.indexOf(letter) !==-1){
+            this.dummysentence[this.name.indexOf(letter)] = letter;
+            guessword.textContent= this.dummysentence;
+    
+        }
     }
-    // playmusic(){
-    //     this.media.play();
-    // }
-
+    playmusic(){
+        this.media.play();
+    }
 }
 
-var wordlist = ["test", "x", "y","hello","world"];
+var wordlist = ["doom", "skyrim", "mario"];
+var keyes =[];
 rand = Math.floor(Math.random() * wordlist.length);
 var x = new word(wordlist[rand]);
 x.draw();
+//creates new class from wordlist and corrisponding media file
+// function createwords(){
+
+// }
+
+
+document.onkeyup = function(event) {
+    let press = event.key.toLowerCase();
+    if(keyes.indexOf(press) === -1){
+        keyes.push(press);
+        if(x.name.indexOf(press) !== -1){
+            x.rightguess(press);
+        }
+        else{
+            
+        }
+    
+    }
+    
+    
+}
