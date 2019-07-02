2345ac67529f5b3b4d1802412f4a6dcdeab824ea
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-02 01:37:09 -0500

    fixed bug in switch statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -45,7 +45,7 @@ class word {
                 this.gamepic.src = "assets/images/doom_box.jpg"
                 break;
             }
-            case "skyrim":{
+            case "skyrim":{ 
                 this.audio.src = "assets/sound/The_Elder_Scrolls_V_Skyrim_Dragonborn_Theme.mp3";
                 this.gamepic.src = "assets/images/220px-The_Elder_Scrolls_V_Skyrim_cover.jpg";
                 break;
