32e0b4093516da45e4a310988417c33b14aa31ff
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-02 00:55:54 -0500

    Added Documentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,102 @@
-var guessword = document.getElementById("guessspot");
-var underline = document.getElementById("underlines");
-var winloss = document.getElementById("winstate");
-var guesscounter = document.getElementById("guesscounter");
-var canvas = document.getElementById("canvas");
+var guessword = document.getElementById("guessspot");                   //HTML Elements  where the user input is displayed
+var winloss = document.getElementById("winstate");                      //shows the title of the game or displays that you lost
+var guesscounter = document.getElementById("guesscounter");             //counts rmeaining guesses
+var canvas = document.getElementById("canvas");                         //canvas that displays game images
 var my_context = canvas.getContext('2d');
 
 class word {
     constructor(n) {
-        this.name = n;
-        this.guesses = 5;
-        this.rightpicks = 0;
-        this.dummysentence = [];
-        this.audio = new Audio();
-        this.gamepic = new Image();
+        this.name = n;                                                  //the name that the player guesses essential for most of the program
+        this.guesses = 5;                                               //how many guesses the player has
+        this.rightpicks = 0;                                            //counts how many right characters have been pressed to determine completion
+        this.dummysentence = [];                                        //prints blank underscores to hide correct name
+        this.audio = new Audio();                                       //plays music
+        this.gamepic = new Image();                                     //the picture of the game
 
     }
-    clear(){
+    clear(){                                                            //clears out the canvas before every image do avoid overlap
         my_context.fillStyle = "black";
         my_context.fillRect(0, 0, canvas.width, canvas.height);
 
     }
-    draw() {
-        winloss.textContent ="???";
+    draw() {                                                            //draws dummytext at the start of the level
+        
         this.dummysentence = [];
         for (let i = 0; i < this.name.length; i++) {
             this.dummysentence[i] = "_ ";
         }
         guessword.textContent = this.dummysentence;
     }
-    rightguess(letter) { //if player presses a correct key it will redraw the word
-        if (x.name.indexOf(letter) !== -1) {
-            this.dummysentence[this.name.indexOf(letter)] = letter;
-            this.dummysentence[this.name.lastIndexOf(letter)] = letter;
-            guessword.textContent = this.dummysentence;
-
-        }
-        for(let i = 0; i < x.name.length; i++){
-            if(letter === x.name[i]){
-                this.rightpicks++;
+    rightguess(letter) {                                                  //if player presses a correct key it will redraw the corresponding letter
+        if (this.name.indexOf(letter) !== -1) {
+            for(let i = 0; i < this.name.length; i++){
+                if(letter === this.name[i]){
+                    this.dummysentence[i] = letter;
+                    this.rightpicks++;
+                }
             }
         }
-    }
-    playmusic() {
-
-        if (this.name.toLowerCase() === "doom") {
-            this.audio.src = "assets/sound/level1.mp3";
-            this.gamepic.src = "assets/images/doom_box.jpg"
+        guessword.textContent = this.dummysentence;
         }
-        if (this.name.toLowerCase() === "skyrim") {
-            this.audio.src = "assets/sound/The_Elder_Scrolls_V_Skyrim_Dragonborn_Theme.mp3";
-            this.gamepic.src = "assets/images/220px-The_Elder_Scrolls_V_Skyrim_cover.jpg";
-        }
-        if (this.name.toLowerCase() === "mario") {
-            this.audio.src = "assets/sound/Mario-Sheet-Music-Overworld-Main-Theme.mp3";
-            this.gamepic.src = "assets/images/220px-Super_Mario_Bros._box.jpg";
-        }
-        if (this.name.toLowerCase() === "punchout") {
-            this.audio.src = "assets/sound/18 - Training Theme.mp3";
-            this.gamepic.src = "assets/images/mike-tysons-punch-out-nes-featured.jpg";
-
-        }
-        if (this.name.toLowerCase() === "zelda") {
-            this.audio.src = "assets/sound/z1theme.mp3";
-            this.gamepic.src = "assets/images/zelda.jpg";
-        }
-        if (this.name.toLowerCase() === "halflife") {
-            this.audio.src = "assets/sound/01. Hazardous Environments.mp3";
-            this.gamepic.src = "assets/images/ZrV8mkWSiCSpuXtEXyJCdT-320-80.jpg";
-
-        }
-        if (this.name.toLowerCase() === "teamfortress") {
-            this.audio.src = "assets/sound/32169_Team-Fortress-2-Theme.mp3";
-            this.gamepic.src = "assets/images/400px-Team_Fortress_2_Group_Photo.jpg";
-
-        }
-
+    playmusic() {                                                          //selects proper music and img corresponding to name
+        switch(this.name.toLowerCase()){
+            case "doom":{
+                this.audio.src = "assets/sound/level1.mp3";
+                this.gamepic.src = "assets/images/doom_box.jpg"
+                break;
+            }
+            case "skyrim":{
+                this.audio.src = "assets/sound/The_Elder_Scrolls_V_Skyrim_Dragonborn_Theme.mp3";
+                this.gamepic.src = "assets/images/220px-The_Elder_Scrolls_V_Skyrim_cover.jpg";
+                break;
+            }
+            case "mario":{
+                this.audio.src = "assets/sound/Mario-Sheet-Music-Overworld-Main-Theme.mp3";
+                this.gamepic.src = "assets/images/220px-Super_Mario_Bros._box.jpg";
+                break;
+            }
+            case  "punchout":{
+                this.audio.src = "assets/sound/18 - Training Theme.mp3";
+                this.gamepic.src = "assets/images/mike-tysons-punch-out-nes-featured.jpg";
+                break;
+            }
+            case "zelda":{
+                this.audio.src = "assets/sound/z1theme.mp3";
+                this.gamepic.src = "assets/images/zelda.jpg";
+                break;
+            }
+            case "halflife":{
+                this.audio.src = "assets/sound/01. Hazardous Environments.mp3";
+                this.gamepic.src = "assets/images/ZrV8mkWSiCSpuXtEXyJCdT-320-80.jpg";
+                break;
+            }
+            default:{
+                this.audio.src = "assets/sound/32169_Team-Fortress-2-Theme.mp3";
+                this.gamepic.src = "assets/images/400px-Team_Fortress_2_Group_Photo.jpg";
+            }
+        }                                                               
         this.audio.play();
         this.audio.loop = true;
     }
-    guessdisplay() {
-        guesscounter.textContent = this.guesses;
+    guessdisplay() {                                                                    //displays how many guesses the player has
+        guesscounter.textContent = "Guesses Left: " + this.guesses;
 
     }
+    cleanslate(){                                                                       //initializes the first level
+        winloss.textContent ="???";
+        this.clear();
+        this.draw();
+        this.playmusic();
+        this.guessdisplay();
+    }
 }
-var usedwords =[];
-var wordlist = ["doom", "skyrim", "mario", "punchout", "zelda", "halflife", "teamfortress"];
-var keyes = [];
-var rand = Math.floor(Math.random() * wordlist.length);
-var x = new word(wordlist[rand]);
-x.clear();
-x.draw();
-x.playmusic();
-x.guessdisplay();
 
+var wordlist = ["doom", "skyrim", "mario", "punchout", "zelda", "halflife", "teamfortress"];      //list of words used
+var keyes = [];                                                                                   //logs pressed keyes
+var rand = Math.floor(Math.random() * wordlist.length);                                           //rng
+var x = new word(wordlist[rand]);                                                                 //initializes game object
+
+x.cleanslate();                                                                                   //initializes first level
 
 function reroll() {
     x.rightpicks =0;
@@ -105,41 +109,42 @@ function reroll() {
     x.guessdisplay();
 }
 
-function youwin() {
-   
-    if(wordlist.length > 1){
+function youwin() {                                                                                   //draws new image corresponding to game and removes previous level from
+                                                                                                      //the array and resets it once it is empty. also resets parameters for
+    if(wordlist.length > 1){                                                                          //the next level
          wordlist.splice(wordlist.indexOf(x.name), 1);
 
     }
     else{
-        wordlist = ["doom", "skyrim", "mario", "punchout", "zelda", "halflife", "teamfortress"];
+        wordlist = ["doom", "skyrim", "mario", "punchout", "zelda", "Halflife", "teamfortress"];
     }
-    my_context.fillStyle = "black";
-    my_context.fillRect(0, 0, canvas.width, canvas.height);
+    x.clear();
     my_context.drawImage(x.gamepic, canvas.width / 2 - x.gamepic.width / 2, canvas.height / 2 - x.gamepic.height / 2);  
-    winloss.textContent = x.name;
+    winloss.textContent = x.name.toUpperCase();
     reroll();
 
 }
 
-document.onkeyup = function (event) {
+document.onkeyup = function (event) {                                                                   //main controls based on keypress
+    
+
     let press = event.key.toLowerCase();
-    if (x.guesses >= 1) {
-        if (keyes.indexOf(press) === -1) {
+    if (x.guesses >= 1) {                                                                       
+        if (keyes.indexOf(press) === -1) {                                                              //checks if repeat press
             keyes.push(press);
-            if (x.name.indexOf(press) !== -1) {
+            if (x.name.indexOf(press) !== -1) {                                                         //checks if a key matches part of name
                 x.rightguess(press);
-                if (x.rightpicks === x.name.length) {
+                if (x.rightpicks === x.name.length) {                                                   //checks if player has filled out name(possibly can just use dummytext)
                     youwin();
                 }
-            } else {
+            } else {                                                                                    //counts down guesses
                 x.guesses--;
                 x.guessdisplay();
             }
 
         }
     }
-    if (x.guesses === 0) {
+    if (x.guesses === 0) {                                                                      
         winloss.textContent = "YOU LOSE";
         alert("YOU LOSE");
         reroll();
